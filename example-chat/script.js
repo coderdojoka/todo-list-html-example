da4e01fc0df3a2b8e6d57de33bff7fc6e4e3ea8b
david <davidweisspost@gmail.com>
2019-04-16 18:30:43 +0200

    Add chat example

---
+++ example-chat/script.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+
+    $("#msg-input").on("keyup", function(event) {
+        if(event.keyCode === 13) { // On "enter" key press
+            let newMessage = document.createElement("div"); // Create new message box
+            let newMessageText = document.createTextNode($("#msg-input").val()); // Use message from input
+            newMessage.appendChild(newMessageText); // Add message to message box
+            $(".history").append(newMessage); // Add message box to conversation
+            $(".history").children().last().addClass("msg-sent"); // Add css class to added message
+            $(".history").append(document.createElement("br")); // Linebreak
+            $("#msg-input").val(""); // Clear input field
+        }
+    })
+
+});
